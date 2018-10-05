f57b42cd0b8a53318e77adf3dcfdf135f2b78aaa
Sai Ganesh Sunkari <gsunkari@corp.miraclesoft.com>
2018-10-05 08:58:31 -0400

    b

---
+++ src/app/app.component.ts
@@ -0,0 +1,64 @@
+import {Component, ElementRef, ViewChild} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../environments/environment';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  acceptedMimeTypes = [
+    'image/gif',
+    'image/jpeg',
+    'image/png'
+  ];
+
+  @ViewChild('fileInput') fileInput: ElementRef;
+  fileDataUri = '';
+  errorMsg = '';
+
+  constructor(private http: HttpClient) {}
+
+  previewFile() {
+    const file = this.fileInput.nativeElement.files[0];
+    if (file && this.validateFile(file)) {
+
+      const reader = new FileReader();
+      reader.readAsDataURL(this.fileInput.nativeElement.files[0]);
+      reader.onload = () => {
+        this.fileDataUri = reader.result;
+      }
+    } else {
+      this.errorMsg = 'File must be jpg, png, or gif and cannot be exceed 500 KB in size'
+    }
+  }
+
+  uploadFile(event: Event) {
+    event.preventDefault();
+
+    // get only the base64 file and post it
+    if (this.fileDataUri.length > 0) {
+      const base64File = this.fileDataUri.split(',')[1];
+      const data = {'image': base64File};
+      this.http.post(`${environment.apiUrl}/upload-photo`, data)
+        .subscribe(
+          res => {
+            // handle success
+            // reset file input
+            this.fileInput.nativeElement.value = '';
+          },
+          err => {
+            this.errorMsg = 'Could not upload image.';
+          }
+        );
+    }
+
+  }
+
+  validateFile(file) {
+    return this.acceptedMimeTypes.includes(file.type) && file.size < 500000;
+  }
+
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  apiUrl: 'https://kqittwg2pi.execute-api.us-east-1.amazonaws.com/PROD' // i.e.'https://beepboop.execute-api.us-west-2.amazonaws.com/Prod'
+};
